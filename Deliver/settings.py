bd1b1c099d37f4a88a6d487707d26dc46ea74c1d
timibacco <foxxelit9@gmail.com>
2022-05-27 21:23:21 +0100

    security

--- Deliver/settings.py
+++ Deliver/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open(BASE_DIR / 'secret_key.txt') as f:
+with open('customer/secret_key.txt') as f:
     SECRET_KEY = f.read().strip()
 
 
